27e9e14c1d2a64ab19d671b56def644e7a71413c
Brenden Capps <brenden.capps@gmail.com>
2022-11-30 23:37:51 -0600

    Solved first puzzle.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
+import java.io.File
+import java.lang.Integer.max
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    // Make sure we get the correct answer from the example.
+    check(getMaxCalories("exampleData.txt") == 24000)
+
+    // Print out the max.
+    println(getMaxCalories("caloriesInput.txt"))
+}
+
+fun getMaxCalories(input: String): Int {
+    var maxCalories = 0
+    var currentCalories = 0
+    for(calorie in File(input).readLines()) {
+        if(calorie.isBlank()) {
+            maxCalories = max(currentCalories, maxCalories)
+            currentCalories = 0
+        }
+        else {
+            currentCalories += calorie.toInt()
+        }
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    return max(currentCalories, maxCalories)
 }
